6a11072d4c4a480d2fdeb0f1d9f86c2ac6a1d7d8
Julia Nocera <giulz42@gmail.com>
2015-12-15 17:50:22 -0800

    configures bower

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+// server.js
 
+//Base Setup 
+//==============================================================================================
 
+//require packages needed
 var _ = require('lodash');
 var express = require('express');
 var path = require('path');
+// var bodyParser = require('body-parser');
+
+// // configure app to use bodyParser()
+// // lets us get data from a POST
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
 var app = express();
 var server = require('http').Server(app);
